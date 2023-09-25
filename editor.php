5b82b71e4f9cc9fc2e43f24f793202902fbd64de
Treasure <obisiket@yahoo.com>
2023-09-25 07:58:00 +0100

    feat: added tes site and updated editor to extract site from url

--- editor.php
+++ editor.php
@@ -1,9 +1,11 @@
 <?php
+
+$site = $GET['site'] ?? 'site-test';
 //include 'editor.html';
 $html = file_get_contents('editor.html');
 
 //search for html files in demo and my-pages folders
-$htmlFiles = glob('{my-pages/*.html,demo/*\/*.html, demo/*.html}',  GLOB_BRACE);
+$htmlFiles = glob('{site-'.$site.'/*.html,site-'.$site.'/*\/*.html,demo/*\/*.html, demo/*.html}',  GLOB_BRACE);
 $files = '';
 foreach ($htmlFiles as $file) { 
    if (in_array($file, array('new-page-blank-template.html', 'editor.html'))) continue;//skip template files
@@ -21,7 +23,7 @@ foreach ($htmlFiles as $file) {
   $files .= "{name:'$name', file:'$file', title:'$title',  url: '$url', folder:'$folder'},";
 } 
 
-
+$html = str_replace('window.SITE=""', 'window.SITE="site-'.$site.'"', $html);
 //replace files list from html with the dynamic list from demo folder
 $html = str_replace('(pages);', "([$files]);", $html);
 
