c72b1e5e76a06a4878747a98242f1a6dc890d055
Treasure <obisiket@yahoo.com>
2023-09-27 07:34:18 +0100

    feat: updates

--- editor.php
+++ editor.php
@@ -8,6 +8,20 @@ $html = file_get_contents('editor.html');
 
 $is_admin = $site == "template" || $site == "test";
 
+$site_dir = __DIR__ . "/site-{$site}";
+
+$_site = file_get_contents("example.site");
+$_blank = file_get_contents("new-page-blank-template.html");
+
+if (!is_dir($site_dir)) {
+  // Run a condition to check if the Id is an actual wizard ID
+  // Make a CURL request
+  // Use the response
+  mkdir($site_dir, 0777, true);
+  file_put_contents($site_dir .'/index.html', $_blank);
+  file_put_contents($site_dir .'/.site', $_site);
+}
+
 //search for html files in demo and my-pages folders
 $htmlFiles = glob('{site-'.$site.'/*.html,site-'.$site.'/*\/*.html'.
   ( $is_admin ? ',demo/*\/*.html, demo/*.html': '').'}',  GLOB_BRACE);
