510c438a83b5877c1612c33a28b9d0cb41c1b594
Treasure <obisiket@yahoo.com>
2023-10-13 18:25:44 +0100

    feat: added the wizard file

--- cdomain.php
+++ cdomain.php
@@ -8,29 +8,29 @@ $domainName = $_GET['dn']; // Replace with the actual domain name
 
 // Read the existing configuration file
 $configFile = __DIR__ . "/$folderName/.site"; // Replace with the actual path to your config file
-$configSSLFile = __DIR__ . "/$folderName/.site-ssl"; // Replace with the actual path to your config file
+// $configSSLFile = __DIR__ . "/$folderName/.site-ssl"; // Replace with the actual path to your config file
 
 $config = file_get_contents('example.site');
-$configSSL = file_get_contents('example.site');
+// $configSSL = file_get_contents('example.site');
 
 $config = str_replace("site.folder", $folderName, $config);
 $config = str_replace("domain.space", $domainName, $config);
 
-$configSSL = str_replace("site.folder", $folderName, $configSSL);
-$configSSL = str_replace("domain.space", $domainName, $configSSL);
+// $configSSL = str_replace("site.folder", $folderName, $configSSL);
+// $configSSL = str_replace("domain.space", $domainName, $configSSL);
 
 file_put_contents($configFile, $config);
-file_put_contents($configSSLFile, $configSSL);
+// file_put_contents($configSSLFile, $configSSL);
 
 $logFile = __DIR__ . "/certbot_output.log";
 
-$command = "curl http://127.0.0.1:3000/runcertbot?domain=".$domainName;
+$command = "curl http://127.0.0.1:3000/runcertbot?domain=".$domainName."folder=".$folderName;
 $output = shell_exec($command);
 
 // Output the result or handle errors
 if ($output === null) {
     echo "Error executing the command.";
-    unlink($configSSLFile);
+    // unlink($configSSLFile);
     echo $output;
     exit;
 } else {
--- ssl.app.js
+++ ssl.app.js
@@ -4,45 +4,112 @@ const { exec } = require('child_process');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const WEBEDITOR_PATH = '/var/www/html/webeditor';
 
-const refreshApache = (cb) => {
-  const command = "sudo systemctl reload apache2";
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(error);
-    } else {
-      if (cb) cb();
+const execute = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout)
+      }
+    });
+  })
+}
+
+const context = (req, res) => {
+  const $url = url.parse(req.url);
+  const routeKey = `${req.method} ${$url.path}`;
+  const getController = (routers) => routers[routeKey] || routers.fallback;
+
+  const send = ({ status, message, headers }) => {
+    const $status = status || 200;
+    const $headers = {
+      ['Content-Type']: 'text/plain',
+      ...(headers || {}),
     }
-  });
+
+    if ($status === 500) {
+      console.error(message)
+    }
+
+    res.writeHead($status, $headers);
+    res.end(message);
+  }
+
+  return {
+    req,
+    url: {
+      ...$url,
+      routeKey,
+    },
+    res: {
+      ...res,
+      send,
+    },
+    getController 
+  }
+}
+
+const commands = {
+  async reloadApache() {
+    return execute("sudo systemctl reload apache2");
+  },
+  async symLink(folder) {
+    if (!folder) {
+      throw new Error("no file path or destination path provided.");
+    }
+    return execute(`sudo ln -s ${WEBEDITOR_PATH}/${folder}/.site /etc/apache2/sites-enabled/${folder}.conf`);
+  },
+  async generateSSL(domain, email="obisiket@gmail.com") {
+    if (!domain) {
+      throw new Error("no domain provided");
+    }
+
+    return execute(`
+      sudo certbot --apache \
+      -d ${domain} --non-interactive \
+      --agree-tos --email ${email} -v`)
+  }
 }
 
 const server = http.createServer((req, res) => {
-  if (req.url.includes('/runcertbot') && req.method === 'GET') {
-    var query = url.parse(req.url,true).query;
-    const domain = query.domain;
-    const email = "obisiket@gmail.com";
-
-    const command = `sudo certbot certonly --apache -d ${domain} --non-interactive --agree-tos --email ${email}`;
-
-    refreshApache(() => {
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error: ${error}`);
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end(`Error: ${error}`);
-        } else {
-          refreshApache(() => {
-            console.log(`Command output:\n${stdout}`);
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(`Command output:\n${stdout}`);
-          })
+  const ctx = context(req, res);
+  ctx.getController({
+    ["GET /runcertbot"]: async ({ res, url: data }) => {
+      try {
+        const {
+          domain, 
+          folder
+        } = data.query;
+    
+        if (!domain) {
+          throw new Error("no domain provided");
         }
+    
+        if (!configPath) {
+          throw new Error("no config path provided");
+        }
+    
+        await commands.reloadApache();
+        await commands.symLink(folder);
+        await commands.generateSSL(domain);
+        await commands.reloadApache();
+      } catch (error) {
+        res.send({
+          status: 500,
+          message: `Error: ${error}`
+        })
+      } 
+    },
+    fallback: ({ res }) => {
+      return res.send({
+        status: 404,
+        message: "Not Found",
       });
-    })
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
-  }
+    }
+  })(ctx);
 });
 
 server.listen(port, hostname, () => {
