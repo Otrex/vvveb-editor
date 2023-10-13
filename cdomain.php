df79982da0d662e8b6573090ba2c029148777c11
Treasure <obisiket@yahoo.com>
2023-10-13 01:48:28 +0100

    feat: added the wizard file

--- cdomain.php
+++ cdomain.php
@@ -13,17 +13,18 @@ $configSSLFile = __DIR__ . "/$folderName/.site-ssl"; // Replace with the actual 
 $config = file_get_contents('example.site');
 $configSSL = file_get_contents('example.site');
 
-$config = str_replace("folder.site", $folderName, $config);
+$config = str_replace("site.folder", $folderName, $config);
 $config = str_replace("domain.space", $domainName, $config);
 
-$configSSL = str_replace("folder.site", $folderName, $configSSL);
+$configSSL = str_replace("site.folder", $folderName, $configSSL);
 $configSSL = str_replace("domain.space", $domainName, $configSSL);
 
 file_put_contents($configFile, $config);
 file_put_contents($configSSLFile, $configSSL);
 
+$logFile = __DIR__ . "/certbot_output.log";
 $email = "obisiket@gmail.com";
-$command = "sudo certbot certonly --apache -d $domainName --non-interactive --agree-tos --email $email";
+$command = "sudo /usr/bin/certbot certonly --apache -d $domainName --non-interactive --agree-tos --email $email > $logFile 2>&1";
 
 $output = shell_exec($command);
 
